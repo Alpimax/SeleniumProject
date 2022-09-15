1aaa191fcdbebc0a749e38dccd79974e8dea886c
Alpimax <alperrozkann@gmail.com>
2022-09-15 14:31:05 -0400

    get website case

--- src/test/java/com/cydeo/tests/day1/Selenium.java
+++ src/test/java/com/cydeo/tests/day1/Selenium.java
@@ -1,20 +1,25 @@
 package com.cydeo.tests.day1;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Selenium {
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.
 //         for setup
         WebDriver driver = new ChromeDriver();
 //        polymorphism
-        driver.get("https://www.tesla.com/");
+        driver.get("www.google.com/");
 //        get for website.
-
+        driver.findElement(By.name("q")).sendKeys("mkaraas");
+        Thread.sleep(1000);
+        driver.findElement(By.className("gNO89b")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.className("LC20lb MBeuO DKV0Md")).click();
 // test case created.
 
     }
