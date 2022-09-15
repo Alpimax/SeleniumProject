084c692f2e333c2fe144c37d17785d52f6a87367
Alpimax <alperrozkann@gmail.com>
2022-09-15 15:17:05 -0400

    get website case

--- src/test/java/com/cydeo/tests/day1/Selenium.java
+++ src/test/java/com/cydeo/tests/day1/Selenium.java
@@ -13,15 +13,23 @@ public class Selenium {
 //         for setup
         WebDriver driver = new ChromeDriver();
 //        polymorphism
-        driver.get("https://www.google.com/?client=safari");
+        driver.get("https://www.google.com");
 //        get for website.
-        driver.findElement(By.name("q")).sendKeys("mkaraas");
+        driver.findElement(By.name("q")).sendKeys("playstation");
         Thread.sleep(1000);
         driver.findElement(By.className("gNO89b")).click();
         Thread.sleep(1000);
+        driver.findElement(By.className("MUxGbd v0nnCb lyLwlc aLF0Z OSrXXb")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.className("web-store-tab tertiary-tab__link")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.className("psw-fade-in psw-center psw-l-fit-cover")).click();
+        Thread.sleep(2000);
         driver.navigate().back();
         Thread.sleep(1000);
         driver.navigate().forward();
+        Thread.sleep(1000);
+        driver.navigate().refresh();
 
 // test case created.
 
