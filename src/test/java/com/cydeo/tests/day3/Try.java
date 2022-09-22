b52e6ffc27fc33fe2a4fcf91bb772ca02e601d77
Alpimax <alperrozkann@gmail.com>
2022-09-22 15:39:48 -0400

    created

---
+++ src/test/java/com/cydeo/tests/day3/Try.java
@@ -0,0 +1,10 @@
+package com.cydeo.tests.day3;
+
+
+import static com.cydeo.Z_utilities.WebDriverFactory.getDriver;
+
+public class Try  {
+    public static void main(String[] args) {
+        getDriver("edge");
+    }
+}
