6c3f53aab6048cb922e3a8c4f1b9db65ca7895c0
Alpimax <alperrozkann@gmail.com>
2022-09-22 11:40:55 -0400

    created

--- src/test/java/com/cydeo/tests/day3/T1Locators.java
+++ src/test/java/com/cydeo/tests/day3/T1Locators.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day3;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T1Locators {
@@ -10,6 +12,12 @@ public class T1Locators {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-//      2- Go to
+//      2- Go to https;// login1.nextbasecrm.com/
+        driver.get("https;// login1.nextbasecrm.com/");
+//      3-  Enter  incorrect username: "incorrect"
+        WebElement inputUserName = driver.findElement(By.name("USER_LOGIN"));
+        inputUserName.sendKeys("incorrect");
+//      4- Enter incorrect password: "incorrect"
+//        mac : option
     }
 }
