c0abaa2b8521d6330a1acd307fbdce6b7bcc776b
Alpimax <alperrozkann@gmail.com>
2022-09-29 12:07:07 -0400

    test ng  created

--- src/test/java/com/cydeo/tests/day5/testng.java
+++ src/test/java/com/cydeo/tests/day5/testng.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day5;
 
 import com.cydeo.Z_utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,7 +14,8 @@ public class testng {
       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
       driver.get("https://practice.cydeo.com/radio_buttons");
 
-
+    WebElement hockeyButton = driver.findElement(By.xpath("//input[@id='football']"));
+    hockeyButton.click();
 
     }
 
