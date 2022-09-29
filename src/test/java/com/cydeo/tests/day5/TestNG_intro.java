cb4b45e259b9281f0d10c69488ef2bc0f4e2d05b
Alpimax <alperrozkann@gmail.com>
2022-09-29 14:57:07 -0400

    test ng  created

--- src/test/java/com/cydeo/tests/day5/TestNG_intro.java
+++ src/test/java/com/cydeo/tests/day5/TestNG_intro.java
@@ -1,18 +1,38 @@
 package com.cydeo.tests.day5;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_intro {
 
-
-    @Test
-    public void test1() {
-        System.out.println("Test one is running!!!");
+    @Test (priority = 1 )
+    public void test1(){
+        System.out.println("Test1 is running...");
     }
 
-    @Test
-    public void test2() {
-        System.out.println("Test two is running!!!");
-
+    @Test (priority = 2)
+    public void mytest2(){
+        System.out.println("test2 is running...");
     }
+
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("--> Before Method is running");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("-->After Method is running");
+    }
+
+    @BeforeClass
+    public void setUp(){
+        System.out.println("--> Before Class is running");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        System.out.println("--> After class is running");
+    }
+
 }
