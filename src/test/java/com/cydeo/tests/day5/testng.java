5779673077c79632c81726f4e167d491e0f7e148
Alpimax <alperrozkann@gmail.com>
2022-09-29 11:35:54 -0400

    test ng  created

--- src/test/java/com/cydeo/tests/day5/testng.java
+++ src/test/java/com/cydeo/tests/day5/testng.java
@@ -1,4 +1,14 @@
 package com.cydeo.tests.day5;
 
+import com.cydeo.Z_utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class testng {
+    public static void main(String[] args) {
+      WebDriver drive =  WebDriverFactory.getDriver("chrome");
+      drive.manage().window().maximize();
+      drive.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+    }
 }
