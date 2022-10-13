d9351062f707503087f7a64bf2445a5d5b213d23
Alpimax <alperrozkann@gmail.com>
2022-10-13 10:38:26 -0400

    added base package

--- src/test/java/com/cydeo/tests/DAy9/T1_CRM_Login.java
+++ src/test/java/com/cydeo/tests/DAy9/T1_CRM_Login.java
@@ -1,7 +1,16 @@
 package com.cydeo.tests.DAy9;
 
-public class T1_CRM_Login {
+import com.cydeo.Z_utilities.WebDriverFactory;
+import com.cydeo.tests.base.TestBase;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_CRM_Login extends TestBase {
     public static void main(String[] args) {
+
 
 
 
---
+++ src/test/java/com/cydeo/tests/base/TestBase.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.base;
+
+import com.cydeo.Z_utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
+
+    protected WebDriver driver;
+    protected String browser;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.getDriver(browser);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
