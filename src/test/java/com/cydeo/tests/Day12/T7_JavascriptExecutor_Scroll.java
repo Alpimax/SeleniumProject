5b4ad1f3f598249e2540a2a8a96b3eb261a55e6b
Alpimax <alperrozkann@gmail.com>
2022-10-18 12:12:55 -0400

    Day 12 created

--- src/test/java/com/cydeo/tests/Day12/T7_JavascriptExecutor_Scroll.java
+++ src/test/java/com/cydeo/tests/Day12/T7_JavascriptExecutor_Scroll.java
@@ -1,4 +1,21 @@
 package com.cydeo.tests.Day12;
 
+import org.testng.annotations.Test;
+
 public class T7_JavascriptExecutor_Scroll {
+
+    @Test
+    public void task7_javascriptexecutor_scroll_test() {
+
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/large
+
+
+        //3- Scroll down to “Cydeo” link
+
+
+        //4- Scroll up to “Home” link
+
+
+    }
 }
