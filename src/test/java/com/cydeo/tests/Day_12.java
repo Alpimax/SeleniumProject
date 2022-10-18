9a414969cad8f3472b0d35260eb11020274df615
Alpimax <alperrozkann@gmail.com>
2022-10-18 11:40:49 -0400

    Day 12 created

--- src/test/java/com/cydeo/tests/Day_12.java
+++ src/test/java/com/cydeo/tests/Day_12.java
@@ -1,5 +1,11 @@
 package com.cydeo.tests;
 
+import com.cydeo.Z_utilities.BrowserUtils;
+import com.cydeo.Z_utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class Day_12 {
@@ -8,21 +14,21 @@ public class Day_12 {
 
         //1- Open a chrome browser
         //2- Go to: https://practice.cydeo.com/
+        Driver.getDriver().get("https://practice.cydeo.com/");
 
-
-
-        //3- Scroll down to “Powered by CYDEO”
-
+        // Create an Actions object to use methods coming from Actions class
+        Actions actions = new Actions(Driver.getDriver());
 
         //4- Scroll using Actions class “moveTo(element)” method
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
 
+        actions.moveToElement(cydeoLink).perform();
 
-
-//        1- Continue from where the Task 4 is left in the same test.
-
-
-
+        BrowserUtils.sleep(3);
 //        2- Scroll back up to “Home” link using PageUP button
+        actions.sendKeys(Keys.PAGE_UP,Keys.PAGE_UP,Keys.PAGE_UP).perform();
 
+        Driver.closeDriver();
+    }
     }
 }
