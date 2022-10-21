5fde8a23b61ba2001cdd14cdccfd2ed731182924
Alpimax <alperrozkann@gmail.com>
2022-10-21 11:33:19 -0400

    Day 12 created

--- src/test/java/com/cydeo/tests/Day13/Pom.java
+++ src/test/java/com/cydeo/tests/Day13/Pom.java
@@ -1,4 +1,18 @@
 package com.cydeo.tests.Day13;
 
+import com.cydeo.Z_utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Pom {
+    public Pom() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (xpath = "//strong[text()='Done!']")
+    public WebElement doneMessega;
+
+    @FindBy (xpath =  "//img[@alt='square pants']")
+    public WebElement spongeBobImage;
 }
