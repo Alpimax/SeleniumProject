0ec3f47ad71e7443315ae1c66dac2e135c4e32b9
Alpimax <alperrozkann@gmail.com>
2022-10-24 17:57:58 -0400

    Some practice created

--- src/test/java/com/cydeo/tests/homeWork/HomeWork.java
+++ src/test/java/com/cydeo/tests/homeWork/HomeWork.java
@@ -1,11 +1,16 @@
 package com.cydeo.tests.homeWork;
 
+import com.cydeo.Z_utilities.Driver;
+import org.apache.hc.core5.util.Asserts;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomeWork {
 
     @Test
     public void urlVerification(){
-
+        Driver.getDriver().navigate().to("https://practice.cydeo.com");
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains("cydeo"));
     }
 }
